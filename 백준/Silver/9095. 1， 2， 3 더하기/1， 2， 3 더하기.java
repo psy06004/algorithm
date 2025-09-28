1cd8f3ef7de40229e2b7462e50fe596b717225a5
psy06004 <psy06004@gmail.com>
2025-09-28 16:51:01 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 0; t < T; t++) {
+			int N = Integer.parseInt(br.readLine());
+			int[] dp = new int[N+1];
+			
+			for (int i = 1; i < N+1; i++) {
+				if(i == 1) {
+					dp[i] = 1;
+					continue;
+				}
+				if(i == 2) {
+					dp[i] = 2;
+					continue;
+				}
+				if(i == 3) {
+					dp[i] = 4;
+					continue;
+				}
+				dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+			}
+			
+			System.out.println(dp[N]);
+		}
+	}
+}
