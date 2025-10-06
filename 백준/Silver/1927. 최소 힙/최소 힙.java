ed74cd41d6940471db5e2e752a4e923de44959f5
psy06004 <psy06004@gmail.com>
2025-10-06 18:54:17 +0900

    [Silver II] Title: 최소 힙, Time: 284 ms, Memory: 26508 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			int x = Integer.parseInt(br.readLine());
+			
+			if(x > 0) {
+				pq.add(x);
+			}
+			if(x == 0) {
+				if(pq.isEmpty())
+					sb.append(0).append("\n");
+				else
+					sb.append(pq.poll()).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+}
