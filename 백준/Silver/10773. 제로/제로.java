2ab6c2d684bc8042f9fa9198b273645b65c89887
psy06004 <psy06004@gmail.com>
2026-01-11 16:45:50 +0900

    [Silver IV] Title: 제로, Time: 200 ms, Memory: 23712 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Stack;
 
 public class Main {
@@ -9,21 +10,19 @@ public class Main {
 
         int K = Integer.parseInt(br.readLine());
 
-        Stack<Integer> stack = new Stack<>();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        int result = 0;
 
         for (int i = 0; i < K; i++) {
             int n = Integer.parseInt(br.readLine());
             if(n != 0){
-                stack.add(n);
+                stack.push(n);
+                result += stack.peek();
             }else{
-                stack.pop();
+                result -= stack.pop();
             }
-        }
 
-        int result = 0;
-
-        while (!stack.isEmpty()){
-            result += stack.pop();
         }
 
         System.out.println(result);
