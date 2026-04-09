480e7ca80f3a10431563dc6536b2c22153a0f522
psy06004 <psy06004@gmail.com>
2026-04-10 00:22:54 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        
+        int answer = 1;
+        
+        for (int num : map.values()) {
+            answer *= (num + 1);
+        }
+        return answer - 1;
+    }
+}
